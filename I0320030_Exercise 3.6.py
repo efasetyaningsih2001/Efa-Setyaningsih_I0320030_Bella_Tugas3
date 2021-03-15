f56bc0f2746402d294b6db269091a78bfe3947d5
Efa Setyaningsih <efasetyaningsih2001@gmail.com>
2021-03-16 01:59:00 +0700

    Exercise 1-10

---
+++ I0320030_Exercise 3.6.py
@@ -0,0 +1,7 @@
+#Update dictionary python
+
+dict = {'name':'Zara','Age':7,'Class':'First'}
+dict['Age'] =8                    #Mengubah entri yang sudah ada
+dict['School'] ="DPS School"      #Menambahkan entri baru
+print("dict['Age']: ",dict['Age'])
+print("dict['School']: ",dict['School'])
